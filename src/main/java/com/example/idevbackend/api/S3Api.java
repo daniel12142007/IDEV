9d95da1579bea982410e85b45d6cd65f49925e6c
daniel12142007 <danijelahatzanov@gmail.com>
2024-09-06 14:30:49 +0600

    Adding S3 and updating Subjects to UX/UI

---
+++ src/main/java/com/example/idevbackend/api/S3Api.java
@@ -0,0 +1,48 @@
+package com.example.idevbackend.api;
+
+import com.example.idevbackend.services.S3FileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/s3")
+public class S3Api {
+    private final S3FileService appService;
+
+    @GetMapping("/files")
+//    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<List<String>> listFiles() {
+        return appService.listFiles();
+    }
+
+    @GetMapping("video/{filename}")
+    public ResponseEntity<InputStreamResource> viewPdf(@PathVariable String filename) {
+        return appService.viewVideo(filename);
+    }
+
+    @GetMapping("image/{filename}")
+    public ResponseEntity<InputStreamResource> viewImage(@PathVariable String filename) throws IOException {
+        return appService.viewImage(filename);
+    }
+
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+//    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public String upload(@RequestParam("file") MultipartFile file) throws IOException {
+        return appService.upload(file);
+    }
+
+    @DeleteMapping("/delete")
+//    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public Map<String, String> delete(@RequestParam String fileName) {
+        return appService.delete(fileName);
+    }
+}
